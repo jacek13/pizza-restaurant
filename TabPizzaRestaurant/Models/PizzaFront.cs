c488ab461d6aaad999a0b2357ab521e7f2752eca
jacek13 <micjac2000@o2.pl>
2022-05-16 00:41:42 +0200

    Added sorting of historical orders. Improving the form for editing the pizza.

--- TabPizzaRestaurant/Models/PizzaFront.cs
+++ TabPizzaRestaurant/Models/PizzaFront.cs
@@ -8,26 +8,36 @@ namespace TabPizzaRestaurant.Models
 {
     public class PizzaFront
     {
+        [Display(Name = "Cena")]
         [Required]
         [DataType(DataType.Currency)]
+        [Range(0, 1000, ErrorMessage = "Zła wartość! przedział: (0-1000).")]
         public int price { get; set; }
 
+        [Display(Name = "Koszt")]
         [Required]
         [DataType(DataType.Currency)]
+        [Range(0, 1000, ErrorMessage = "Zła wartość! przedział: (0-1000).")]
         public int cost { get; set; }
 
+        [Display(Name = "Dostępność")]
         [Required]
         public bool isAvailable { get; set; }
 
+        [Display(Name = "Nazwa")]
         [Required]
-        [StringLength(30, ErrorMessage = "Name is too long.")]
-        [MinLength(3, ErrorMessage = "Name is too short")]
+        [StringLength(30, ErrorMessage = "Za Długie.")]
+        [MinLength(3, ErrorMessage = "Za krótkie")]
         public string type { get; set; }
 
+        [Display(Name = "Rozmiar")]
         [Required]
+        [Range(16, 64, ErrorMessage = "Zły rozmiar! przedział: (16-64).")]
         public int size{ get; set; }
 
+        [Display(Name = "Punkty")]
         [Required]
+        [Range(0, 100, ErrorMessage = "Zła wartość! przedział: (0-100).")]
         public int points { get; set; }
     }
 }
